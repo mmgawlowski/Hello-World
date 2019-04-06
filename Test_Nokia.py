ef71096c2cafc38d70f11f9f69d8c4b0b208664f
Maciej Gawłowski <46463400+mmgawlowski@users.noreply.github.com>
2019-04-06 15:41:53 +0200

    Add files via upload

---
+++ Test_Nokia.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+browser = webdriver.Chrome()
+browser.maximize_window()
+
+browser.get('https://google.com')
+
+element = wait(browser, 10).until(
+EC.presence_of_element_located((By.NAME, 'q')))
+
+search_form = browser.find_element_by_name('q')
+search_form.send_keys('Dlaczego warto pracować w Nokii')
+search_form.submit()
+time.sleep(3)
+
+browser.get('https://nokiawroclaw.pl')
+
+element = wait(browser, 10).until(
+EC.element_to_be_clickable((By.LINK_TEXT, 'Dołącz do nas')))
+
+browser.find_element_by_link_text('Dołącz do nas').click()
+
+element = wait(browser, 10).until(
+EC.presence_of_element_located((By.XPATH, '//span[text()="Zobacz wszystkie oferty"]')))
+
+button_zobacz_oferty = browser.find_element_by_xpath('//span[text()="Zobacz wszystkie oferty"]')
+browser.execute_script('arguments[0].scrollIntoView({behavior: "smooth", block: "center"});', button_zobacz_oferty)
+
+element = wait(browser, 10).until(
+EC.visibility_of_element_located((By.XPATH, '//span[text()="Zobacz wszystkie oferty"]')))
+time.sleep(2)
+
+browser.find_element_by_xpath('//span[text()="Zobacz wszystkie oferty"]').click()
+
+element = wait(browser, 10).until(
+EC.title_contains('Oferty pracy'))
+
+browser.find_element_by_xpath('//span[text()="Typ"]').click()
+browser.find_element_by_xpath('//li[text()="Full-Time"]').click()
+browser.find_element_by_xpath('//span[text()="Obszar"]').click()
+browser.find_element_by_xpath('//li[text()="Testing/Integration & Verification"]').click()
+browser.find_element_by_xpath('//button[@class="btn btn--red btn--icon"]').click()
+time.sleep(2)
+browser.find_element_by_link_text('Test Automation Engineer').click()
+
+element = wait(browser, 10).until(
+EC.title_contains('Test Automation Engineer'))
+
+browser.find_element_by_xpath('//a[@class="btn btn--red"]').click()
+
+element = wait(browser, 10).until(
+EC.title_contains('Test Automation Engineer'))
+
+browser.find_element_by_xpath('//span[text()="Apply for Job"]').click()
+
+element = wait(browser, 10).until(
+EC.visibility_of_element_located((By.NAME, 'User.email')))
+
+browser.find_element_by_name('User.email').send_keys('example@gmail.com')
+time.sleep(5)
+
+browser.find_element_by_xpath('//div[@class="submit_cancel jCloseModal"]').click()
+time.sleep(3)
+
+browser.quit()
+quit()
